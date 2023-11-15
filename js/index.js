77a1651aecf0c5af871d0381a0bddc23725e7db3
Chorong <from2chorong@gmail.com>
2023-11-15 15:17:33 +0900

    Upload portfolio

--- js/index.js
+++ js/index.js
@@ -1,27 +1,43 @@
 $(document).ready(function () {
-	ScrollTrigger.refresh();
 
-	gsap.registerPlugin(ScrollTrigger);
-	
-	let circleMove = gsap.timeline(),
-	pinDistance = $(".text-box").outerWidth()
-	circleMove
-		.to(".text-box", { fontSize: "16px" })
-	
-	ScrollTrigger.create({
-		animation: circleMove,
-		trigger: ".text-box",
-		start: "top top",
-		end: "+=" + (pinDistance * 2),
-		pin: true,
-		scrub: true,
+
+	let cont = document.querySelector(".img-box");
+	let scrollTween = gsap.to(".item", {
+		ease: "none",
+		scrollTrigger: {
+			trigger: cont,
+			pin: cont,
+			start: "center center",
+			end: "+=" + 8000,
+			scrub: 0.1,
+		}
 	});
+
+	let targets = gsap.utils.toArray(".item");
+	targets.forEach((item, index) => {
+		gsap
+			.timeline({
+				ease: "none",
+				scrollTrigger: {
+					trigger: item,
+					containerAnimation: scrollTween,
+					start: "top center", // Adjust as needed
+					end: "bottom center", // Adjust as needed
+					pin: true,
+					scrub: true,
+				}
+			})
+			.fromTo(
+				item,
+				{ yPercent: 100 * index, opacity: 1 },
+				{ yPercent: 0, opacity: 1 }
+			);
+	});
+// 아시발모르겟다
+
 
 	$(".mode-button").on("click", function () {
 		$(".scroll-wrap, .mode-button").toggleClass("dark light");
 	});
 
-	$(".list-button").on("click", function () {
-		
-	})
-})
+});
