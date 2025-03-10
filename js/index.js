bd262d908016cc6884e3a01bad635834e21287c3
Chorong <140467025+from2chorong@users.noreply.github.com>
2025-03-11 04:02:47 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,273 +1,162 @@
-const $container = document.querySelector('.container');
-let wraps = [];
-
-function renderList() {
-    const fragment = document.createDocumentFragment();
+$(document).ready(function () {
+  function renderList() {
+    const $lists = $('.lists');
+    let $item; 
 
     lists.forEach(list => {
-        const item = document.createElement('div');
-        item.classList.add('list');
+      $item = $('<div>').addClass('list');
+      const $link = $('<a>').attr({ href: list.link, target: '_blank' });
 
-        const link = document.createElement('a');
-        link.href = list.link;
-        link.target = '_blank';
+      const $text = $('<div>').addClass('text').append(
+        $('<span>').text(list.type),
+        $('<h5>').text(list.name)
+      );
+      const $figure = $('<figure>').addClass('img').append(
+        $('<img>').attr({ src: `/portfolio/images/projects/${list.image}.jpg`, alt: list.img })
+      );
 
-        const figure = document.createElement('figure');
-        figure.classList.add('img');
-
-        const img = document.createElement('img');
-        img.src = `/portfolio/images/projects/${list.image}.jpg`;
-        img.alt = list.img;
-        img.loading = 'lazy';
-
-        figure.append(img);
-
-        const text = document.createElement('div');
-        text.classList.add('text');
-
-        const type = document.createElement('span');
-        type.textContent = list.type;
-
-        const name = document.createElement('h6');
-        name.textContent = list.name;
-
-        text.append(type, name);
-
-        link.append(figure, text);
-        item.append(link);
-        fragment.appendChild(item);
+      $link.append($text, $figure);
+      $item.append($link, $('<span class="hover"></span>'));
+      $lists.append($item);
     });
 
-    $container.appendChild(fragment);
-}
-renderList();
+    $(window).on('load resize', function() {
+      setTimeout(() => {
+        requestAnimationFrame(() => {
+          $('.list').each(function () {
+            let listWidth = $(this)[0].getBoundingClientRect().width;
+            let listHeight = $(this)[0].getBoundingClientRect().height;
 
-function onRenderListComplete() {
-    initAnimation();
-}
+            linesSize(listHeight);
+            listHover(listWidth, listHeight);
+          });
+        });
+      }, 100);
+    });
+  }
 
-function throttle(func, limit) {
-    let lastFunc;
-    let lastRan;
-    return function (...args) {
-        const context = this;
-        if (!lastRan) {
-            func.apply(context, args);
-            lastRan = Date.now();
-        } else {
-            clearTimeout(lastFunc);
-            lastFunc = setTimeout(function () {
-                if (Date.now() - lastRan >= limit) {
-                    func.apply(context, args);
-                    lastRan = Date.now();
-                }
-            }, limit - (Date.now() - lastRan));
+  function linesSize(listHeight) {
+    const listsColumns = $('.lists').css('grid-template-columns').split(' ').length;
+    $('.lines').css({ 'background-size': `calc((100% - 2px) / ${listsColumns}) ${listHeight}px` });
+  }
+
+  function listHover(listWidth, listHeight) {
+    const $list = $('.list');
+
+    $list.each(function () {
+      $(this).find('.hover').css({ 'left': -listWidth, 'top': -listHeight });
+      $(this).off();
+      $(this).on('mouseenter mouseleave focusin focusout', function (e) {
+        var x, y, type;
+
+        x = Math.floor(e.pageX - $(this).offset().left);
+        y = Math.floor(e.pageY - $(this).offset().top);
+
+        x = (e.pageX - $(this).offset().left - (listWidth / 2)) * (listWidth > listHeight ? (listHeight / listWidth) : 1);
+        y = (e.pageY - $(this).offset().top - (listHeight / 2)) * (listHeight > listWidth ? (listWidth / listHeight) : 1);
+
+        type = (e.type == 'mouseenter' || e.type == 'focusin') ? true : false;
+
+        spanMove($(this).find('.hover'), type, x, y);
+      });
+    });
+
+    function spanMove(tgObj, overType, x, y) {
+      var startX, startY, tgX, tgY;
+      var direction = Math.round((((Math.atan2(y, x) * (180 / Math.PI)) + 180) / 90) + 3) % 4;
+      if (overType) {
+        switch (direction) {
+          case 0:
+            startX = 0;
+            startY = -listHeight;
+            break;
+          case 1:
+            startX = listWidth;
+            startY = 0;
+            break;
+          case 2:
+            startX = 0;
+            startY = listHeight;
+            break;
+          case 3:
+            startX = -listWidth;
+            startY = 0;
+            break;
+          default:
+            startX = -listHeight;
+            startY = 0;
+            break;
         }
-    };
-}
-
-function initAnimation() {
-    const $items = gsap.utils.toArray($container.children);
-
-	function setupWraps() {
-		wraps = [];
-        const containerRect = $container.getBoundingClientRect();
-
-        for (let i = 0; i < $items.length; i++) {
-            const itemRect = $items[i].getBoundingClientRect();
-            const min = containerRect.top - itemRect.bottom;
-            const max = containerRect.bottom - itemRect.bottom;
-            const wrap = gsap.utils.wrap(min, max);
-
-            wraps.push(wrap);
+        tgX = 0;
+        tgY = 0;
+        tgObj.stop().css({ 'left': startX, 'top': startY }).animate({ 'left': tgX, 'top': tgY }, { queue: false, duration: 400, easing: 'easeOutExpo' });
+      } else {
+        switch (direction) {
+          case 0:
+            tgX = 0;
+            tgY = -listHeight;
+            break;
+          case 1:
+            tgX = listWidth;
+            tgY = 0;
+            break;
+          case 2:
+            tgX = 0;
+            tgY = listHeight;
+            break;
+          case 3:
+            tgX = -listWidth;
+            tgY = 0;
+            break;
+          default:
+            tgX = listWidth;
+            tgY = 0;
+            break;
         }
+        tgObj.stop().animate({ 'left': tgX, 'top': tgY }, { queue: false, duration: 400, easing: 'easeOutExpo' });
+      }
     }
+  }
 
-    setupWraps();
+  renderList();
 
-	Observer.create({
-        preventDefault: false,
-		target: $container,
-		onChange: ({ deltaY, isDragging, event }) => {
-			if (event.type === 'wheel') {
-				event.preventDefault();
-			}
+  function filterLists() {
+    const isProposalChecked = $('#proposal').is(':checked');
+    const isWebsiteOrGdwebChecked = $('#website').is(':checked') || $('#gdweb').is(':checked');
 
-			const d = event.type === 'wheel' ? -deltaY : deltaY;
-			const y = isDragging ? d * 5 : d * 10;
+    $('#website, #gdweb').prop('disabled', isProposalChecked);
+    $('#proposal').prop('disabled', isWebsiteOrGdwebChecked);
 
-			for (let i = 0; i < $items.length; i++) {
-				gsap.to($items[i], {
-					duration: 1,
-					ease: 'power4.out',
-					y: `+=${y}`,
-					modifiers: {
-						y: gsap.utils.unitize(wraps[i]),
-					},
-				});
-			}
-		},
+    const checkedFilters = [];
+
+    $('.keywords input[type="checkbox"]:checked').each(function () {
+      checkedFilters.push($(this).attr('id'));
     });
 
-		
-	window.addEventListener('resize', () => {
-		setupWraps();
-		resetScrollAnimation();
-	});
+    $('.list').each(function () {
+      const keywords = lists[$(this).index()].keywords;
+      const matchesFilters = checkedFilters.length === 0 || checkedFilters.every(filter => keywords.includes(filter));
 
-	function resetScrollAnimation() {
-		const $items = gsap.utils.toArray($container.children);
-		for (let i = 0; i < $items.length; i++) {
-			gsap.set($items[i], {
-				clearProps: 'y',
-			});
-		}
-	}
+      if (matchesFilters) {
+        $(this).stop().show();
+      } else {
+        $(this).stop().hide();
+      }
+    });
+  }
 
-	function imgMove() {
-		let isMouseInside = false;
-		let isMouseMoving = false;
+  $('.keywords input[type="checkbox"]').change(function () {
+    filterLists();
+  });
 
-		const move = throttle(function (e) {
-			if (!isMouseInside || !isMouseMoving) return;
-
-			const centerX = window.innerWidth / 2;
-			const centerY = window.innerHeight / 2;
-
-			$items.forEach(function ($item) {
-				const rect = $item.getBoundingClientRect();
-				const containerCenterX = rect.left + rect.width / 2;
-				const containerCenterY = rect.top + rect.height / 2;
-
-				const dxFromCenter = containerCenterX - e.clientX;
-				const dyFromCenter = containerCenterY - e.clientY;
-
-				const distanceRatioX = dxFromCenter / centerX;
-				const distanceRatioY = dyFromCenter / centerY;
-
-				const rotationX = distanceRatioY * 10;
-				const rotationY = distanceRatioX * 10;
-
-				const $text = $item.querySelector('.text');
-				if (!$text) return;
-
-				const $h6 = $text.querySelector('h6');
-				if (!$h6) return;
-
-				let $shadow = $text.querySelector('.shadow');
-				if (!$shadow) {
-					$shadow = document.createElement('div');
-					$shadow.classList.add('shadow');
-					const $h6Clone = $h6.cloneNode(true);
-					$shadow.appendChild($h6Clone);
-					$text.appendChild($shadow);
-				}
-
-				$shadow.style.left = `${-dxFromCenter}px`;
-				$shadow.style.top = `${-dyFromCenter}px`;
-
-				gsap.to($item, {
-					duration: 0.5,
-					rotateX: rotationX,
-					rotateY: -rotationY,
-					ease: 'power4.out',
-				});
-
-				gsap.to($text, {
-					duration: 0.5,
-					rotateX: -(rotationX * 2),
-					rotateY: (rotationY * 2),
-					ease: 'power4.out',
-				});
-			});
-		}, 50);
-
-		const leave = function () {
-			$items.forEach(function ($item) {
-				const $text = $item.querySelector('.text');
-				gsap.to($item, {
-					duration: 0.5,
-					rotateX: 0,
-					rotateY: 0,
-					ease: 'power4.out',
-				});
-
-				gsap.to($text, {
-					duration: 0.5,
-					rotateX: 0,
-					rotateY: 0,
-					ease: 'power4.out',
-				});
-			});
-		};
-
-		const enterHandler = function () {
-			isMouseInside = true;
-			isMouseMoving = true;
-			$container.addEventListener('mousemove', move);
-		};
-
-		const leaveHandler = function () {
-			isMouseInside = false;
-			isMouseMoving = false;
-			$container.removeEventListener('mousemove', move);
-			leave();
-		};
-
-		$container.addEventListener('mouseenter', enterHandler);
-		$container.addEventListener('mouseleave', leaveHandler);
-
-	}
-	if (window.innerWidth > 768) {
-		imgMove();
-	}
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-    onRenderListComplete();
-});
-
-function handleShadow($text, action) {
-    const $shadow = $text.querySelector('.shadow');
-    if ($shadow) {
-        if (action === 'add') {
-            $shadow.classList.add('circle');
-        } else if (action === 'remove') {
-            $shadow.classList.remove('circle');
-        }
+  $('.mode input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      $('.mode span').css('left', 'calc(50% + 2.5px)');
+      $('main').removeClass('light').addClass('dark');
+    } else {
+      $('.mode span').css('left', '0');
+      $('main').removeClass('dark').addClass('light');
     }
-}
+  });
 
-const $texts = document.querySelectorAll('.text');
-
-$texts.forEach($text => {
-    $text.addEventListener('mouseover', function () {
-		handleShadow($text, 'add');
-    });
-
-    $text.addEventListener('mouseleave', function() {
-		handleShadow($text, 'remove');
-    });
-});
-
-function toLeft() {
-	var rolling = document.querySelector('.rolling .inner');
-	var pElements = rolling.querySelectorAll('p');
-    var leftW = pElements[0].offsetWidth || 100;
-
-    setInterval(function () {
-        rolling.style.transition = 'transform 20s linear';
-        rolling.style.transform = `translateX(-${leftW}px)`;
-
-        setTimeout(function () {
-            rolling.style.transition = 'none';
-            rolling.style.transform = 'translateX(0)';
-            rolling.appendChild(pElements[0]);
-            pElements = rolling.querySelectorAll('p');
-        }, 20000);
-    }, 10);
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    toLeft();
 });
--- js/lists.js
+++ js/lists.js
@@ -1,242 +1,362 @@
 let lists = [
 	{
 		type: '웹사이트',
-		name: 'Buil',
-		image: 'buil',
-		link: 'https://builsf.com/'	
+		name: '하림지주',
+		image: 'harim',
+		link: 'https://harimholdings.com/kr/',
+		keywords: [
+			'website', 'gdweb', 'gsap'
+		]
 	},
 	{
 		type: '웹사이트',
 		name: 'EBS Media',
 		image: 'ebs',
-		link: 'https://ebsmedia.kr/'
+		link: 'https://ebsmedia.kr/',
+		keywords: [
+			'website', 'gdweb', 'gsap'
+		]
 	},
 	{
 		type: '웹사이트',
 		name: 'ZEUS',
 		image: 'zeus',
-		link: 'https://www.globalzeus.com/kr/'
+		link: 'https://www.globalzeus.com/kr/',
+		keywords: [
+			'website'
+		]
 	},
 	{
 		type: '웹사이트',
 		name: 'OTO',
 		image: 'oto',
-		link: 'http://www.oto.kr/'	
-	},
-	{
-		type: '웹사이트',
-		name: 'Sharp Aviation K',
-		image: 'sharp',
-		link: 'https://www.sharp.co.kr/kr/index.asp'	
-	},
-	{
-		type: '웹사이트',
-		name: 'Basgenbio',
-		image: 'basgen',
-		link: 'https://basgenbio.com/kr/'	
+		link: 'http://www.oto.kr/',
+		keywords: [
+			'website'
+		]
 	},
 	{
 		type: '웹사이트',
 		name: 'AJ Energy',
 		image: 'aj',
-		link: 'https://www.ajenergy.co.kr/'	
+		link: 'https://www.ajenergy.co.kr/',
+		keywords: [
+			'website', 'gdweb'
+		]
 	},
 	{
 		type: '웹사이트',
-		name: '하림지주',
-		image: 'harim',
-		link: 'https://harimholdings.com/kr/'
+		name: 'Sharp Aviation K',
+		image: 'sharp',
+		link: 'https://www.sharp.co.kr/kr/index.asp',
+		keywords: [
+			'website', 'gdweb'
+		]	
 	},
 	{
 		type: '웹사이트',
-		name: 'LOGOS',
-		image: 'logos',
-		link: 'https://www.lawlogos.com/'	
-	},
-	{
-		type: '웹사이트',
-		name: '아리씨',
-		image: 'recci',
-		link: 'https://recci.co.kr/'	
-	},
-	{
-		type: '웹사이트',
-		name: '한강식품',
-		image: 'hangang',
-		link: 'https://hangangfood.com/'	
-	},
-	{
-		type: '웹사이트',
-		name: 'Clavis',
-		image: 'clavis',
-		link: 'https://www.claviscorp.com/kr/'	
+		name: 'Basgenbio',
+		image: 'basgen',
+		link: 'https://basgenbio.com/kr/',
+		keywords: [
+			'website', 'gdweb', 'gsap'
+		]
 	},
 	{
 		type: '웹사이트',
 		name: 'NGeneBio',
 		image: 'ngene',
-		link: 'https://www.ngenebio.com/kr/'	
-	},
-	{
-		type: '웹사이트',
-		name: 'Kornic Automation',
-		image: 'kornic',
-		link: 'https://www.kornic.co.kr/kr/'	
+		link: 'https://www.ngenebio.com/kr/',
+		keywords: [
+			'website'
+		]
 	},
 	{
 		type: '웹사이트',
 		name: 'Sugentech',
 		image: 'sugentech',
-		link: 'http://sugentech-n.webmoa21.co.kr/kr/'	
+		link: 'http://sugentech-n.webmoa21.co.kr/kr/',
+		keywords: [
+			'website', 'gsap'
+		]
 	},
 	{
 		type: '웹사이트',
-		name: 'Illimis therapeutics',
-		image: 'illimis',
-		link: 'https://illimistx.com/kr/'	
+		name: 'Clavis',
+		image: 'clavis',
+		link: 'https://www.claviscorp.com/kr/',
+		keywords: [
+			'website', 'gdweb'
+		]
 	},
 	{
 		type: '웹사이트',
-		name: 'Purium',
-		image: 'purium',
-		link: 'https://www.purium.kr/kr/'	
+		name: 'LOGOS',
+		image: 'logos',
+		link: 'https://www.lawlogos.com/',
+		keywords: [
+			'website'
+		]
 	},
 	{
 		type: '웹사이트',
-		name: '한국의료재단',
-		image: 'komef',
-		link: 'https://komef.org/'	
+		name: 'Buil',
+		image: 'buil',
+		link: 'https://builsf.com/',
+		keywords: [
+			'website', 'gdweb', 'gsap'
+		]
 	},
 	{
 		type: '웹사이트',
-		name: 'Bestinnovation',
-		image: 'best',
-		link: 'http://www.bestinnovation.co.kr/kr/'	
+		name: 'SC KOREA',
+		image: 'sckorea',
+		link: 'http://sckorea.webmoa21.co.kr/',
+		keywords: [
+			'website', 'gsap'
+		]
 	},
 	{
 		type: '웹사이트',
-		name: '노원문화원',
-		image: 'nwcc',
-		link: 'https://www.nwcc.or.kr/'	
+		name: '아리씨',
+		image: 'recci',
+		link: 'https://recci.co.kr/sub/company/introduction.asp',
+		keywords: [
+			'website', 'gdweb', 'gsap'
+		]
 	},
 	{
 		type: '웹사이트',
-		name: 'Myriad Life Sciences',
-		image: 'myriad',
-		link: 'https://www.myriadls.com/en/'	
-	},
-	{
-		type: '웹사이트',
-		name: '정인회계법인',
-		image: 'jungin',
-		link: 'http://jungincpa.co.kr/kr/'	
-	},
-	{
-		type: '웹사이트',
-		name: '신백아동복지관',
-		image: 'sbkid',
-		link: 'https://www.sbkid.kr/'	
-	},
-	{
-		type: '웹사이트',
-		name: 'Kaltech',
-		image: 'kaltech',
-		link: 'https://kaltech.jp/jp/'	
-	},
-	{
-		type: '웹사이트',
-		name: '정광엔지니어링',
-		image: 'jungkwang',
-		link: 'http://www.jke.co.kr/kr/'	
+		name: '한강식품',
+		image: 'hangang',
+		link: 'https://hangangfood.com/',
+		keywords: [
+			'website', 'gdweb'
+		]
 	},
 	{
 		type: '웹사이트',
 		name: 'IS Tec',
 		image: 'is',
-		link: 'https://www.istec.co.kr/kr/'	
+		link: 'https://www.istec.co.kr/kr/',
+		keywords: [
+			'website', 'gdweb'
+		]
+	},
+	{
+		type: '웹사이트',
+		name: 'Kornic Automation',
+		image: 'kornic',
+		link: 'https://www.kornic.co.kr/kr/',
+		keywords: [
+			'website', 'gdweb'
+		]
+	},
+	{
+		type: '웹사이트',
+		name: 'Illimis therapeutics',
+		image: 'illimis',
+		link: 'https://illimistx.com/kr/',
+		keywords: [
+			'website'
+		]
+	},
+	{
+		type: '웹사이트',
+		name: 'Purium',
+		image: 'purium',
+		link: 'https://www.purium.kr/kr/',
+		keywords: [
+			'website'
+		]
+	},
+	{
+		type: '웹사이트',
+		name: '한국의료재단',
+		image: 'komef',
+		link: 'https://komef.org/',
+		keywords: [
+			'website'
+		]
+	},
+	{
+		type: '웹사이트',
+		name: '노원문화원',
+		image: 'nwcc',
+		link: 'https://www.nwcc.or.kr/',
+		keywords: [
+			'website'
+		]
+	},
+	{
+		type: '웹사이트',
+		name: '정인회계법인',
+		image: 'jungin',
+		link: 'http://jungincpa.co.kr/kr/',
+		keywords: [
+			'website'
+		]
+	},
+	{
+		type: '웹사이트',
+		name: '신백아동복지관',
+		image: 'sbkid',
+		link: 'https://www.sbkid.kr/',
+		keywords: [
+			'website'
+		]
+	},
+	{
+		type: '웹사이트',
+		name: 'Myriad Life Sciences',
+		image: 'myriad',
+		link: 'https://www.myriadls.com/en/',
+		keywords: [
+			'website'
+		]
+	},
+	{
+		type: '웹사이트',
+		name: 'Kaltech',
+		image: 'kaltech',
+		link: 'https://kaltech.jp/jp/',
+		keywords: [
+			'website'
+		]
+	},
+	{
+		type: '웹사이트',
+		name: '세종정신건강복지센터',
+		image: 'simplus',
+		link: 'https://simplus.kr/indexNew.asp',
+		keywords: [
+			'website'
+		]
+	},
+	{
+		type: '웹사이트',
+		name: '세종자살예방센터',
+		image: 'safety',
+		link: 'https://safety.simplus.kr/index.asp',
+		keywords: [
+			'website'
+		]
 	},
 	{
 		type: '웹사이트',
 		name: 'Stemon',
 		image: 'stemon',
-		link: 'https://www.stemon.co.kr/en/'	
+		link: 'https://www.stemon.co.kr/en/',
+		keywords: [
+			'website', 'gdweb'
+		]
 	},
 	{
 		type: '웹사이트',
 		name: 'Stonebridge',
 		image: 'stone',
-		link: 'https://stonebridgecapital.co.kr/kr/'	
+		link: 'https://stonebridgecapital.co.kr/kr/',
+		keywords: [
+			'website'
+		]
 	},
 	{
 		type: '웹사이트',
-		name: '세종광역정신건강복지센터',
-		image: 'simplus',
-		link: 'https://simplus.kr/indexNew.asp'	
-	},
-	{
-		type: '웹사이트',
-		name: '세종광역시자살예방센터',
-		image: 'safety',
-		link: 'https://safety.simplus.kr/index.asp'	
+		name: 'Bestinnovation',
+		image: 'best',
+		link: 'http://www.bestinnovation.co.kr/kr/',
+		keywords: [
+			'website'
+		]
 	},
 	{
 		type: '웹사이트',
 		name: 'YSL Agency',
 		image: 'ysl',
-		link: 'http://yslagency.com/kr/'	
+		link: 'http://yslagency.com/kr/',
+		keywords: [
+			'website'
+		]
+	},
+	{
+		type: '웹사이트',
+		name: '정광엔지니어링',
+		image: 'jungkwang',
+		link: 'http://www.jke.co.kr/kr/',
+		keywords: [
+			'website'
+		]
 	},
 	{
 		type: '웹사이트',
 		name: '해바라기센터',
 		image: 'sunflower',
-		link: 'https://sunflowercenter.or.kr/'	
+		link: 'https://sunflowercenter.or.kr/',
+		keywords: [
+			'website'
+		]
 	},
 	{
 		type: '웹사이트',
 		name: '행복마루',
 		image: 'hmaru',
-		link: 'https://hmaru.co.kr/'	
+		link: 'https://hmaru.co.kr/',
+		keywords: [
+			'website'
+		]
 	},
 	{
 		type: '웹사이트',
 		name: '원 인터내셔널',
 		image: 'wonitl',
-		link: 'https://wonitl.com/'	
-	},
-	{
-		type: '웹사이트',
-		name: '제일타카',
-		image: 'jitool',
-		link: 'http://www.jitool.com/kr/'	
+		link: 'https://wonitl.com/',
+		keywords: [
+			'website'
+		]
 	},
 	{
 		type: '제안시안',
 		name: 'Celltrion',
 		image: 'celltrion',
-		link: 'http://sian.webmoa21.co.kr/celltrion/b/index.html'
+		link: 'http://sian.webmoa21.co.kr/celltrion/b/index.html',
+		keywords: [
+			'proposal', 'gsap'
+		]
 	},
 	{
 		type: '제안시안',
 		name: '동아ST',
 		image: 'dongast',
-		link: 'http://sian.webmoa21.co.kr/dongast/a/index.html'
+		link: 'http://sian.webmoa21.co.kr/dongast/a/index.html',
+		keywords: [
+			'proposal', 'gsap'
+		]
 	},
 	{
 		type: '제안시안',
 		name: '동아쏘시오홀딩스',
 		image: 'dongasocio',
-		link: 'http://sian.webmoa21.co.kr/donga/b/index.html'
+		link: 'http://sian.webmoa21.co.kr/donga/b/index.html',
+		keywords: [
+			'proposal', 'gsap'
+		]
 	},
 	{
 		type: '제안시안',
 		name: '한국카본',
 		image: 'carbon',
-		link: 'http://sian.webmoa21.co.kr/kcarbon/index.html'
+		link: 'http://sian.webmoa21.co.kr/kcarbon/index.html',
+		keywords: [
+			'proposal', 'gsap'
+		]
 	},
 	{
 		type: '제안시안',
 		name: 'Neooto',
 		image: 'neooto',
-		link: 'http://sian.webmoa21.co.kr/neooto/b/'
+		link: 'http://sian.webmoa21.co.kr/neooto/b/',
+		keywords: [
+			'proposal'
+		]
 	}
 ]
